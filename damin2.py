943b978fa32139e976764ddb8358bd46effde943
mfadli26 <42895640+mfadli26@users.noreply.github.com>
2020-03-31 11:10:47 +0700

    Add files via upload

---
+++ damin2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+from sklearn.cluster import AgglomerativeClustering
+
+dataset = pd.read_csv('dataset.csv')
+X = dataset.iloc[:, [0,3]].values
+
+dendrogram = sch.dendrogram(sch.linkage(X, method  = "ward"))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage ='ward')
+y_hc=hc.fit_predict(X)
+
+plt.scatter(X[y_hc==0, 0], X[y_hc==0, 1], s=100, c='red', label ='Cluster 1')
+plt.scatter(X[y_hc==1, 0], X[y_hc==1, 1], s=100, c='blue', label ='Cluster 2')
+plt.scatter(X[y_hc==2, 0], X[y_hc==2, 1], s=100, c='green', label ='Cluster 3')
+plt.scatter(X[y_hc==3, 0], X[y_hc==3, 1], s=100, c='cyan', label ='Cluster 4')
+plt.title('Clusters of Customers (Hierarchical Clustering Model)')
+plt.xlabel('Annual Income(k$)')
+plt.ylabel('Spending Score(1-100')
+plt.show()
